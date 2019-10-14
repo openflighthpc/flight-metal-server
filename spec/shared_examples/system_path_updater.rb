2360e87aaef89d832c2b0d0f424297efc42a3f3b
william <william.mccumstie@gmail.com>
2019-10-14 15:57:45 +0100

    Extract the updater code into a shared example

--- spec/shared_examples/system_path_updater.rb
+++ spec/shared_examples/system_path_updater.rb
@@ -27,54 +27,61 @@
 # https://github.com/openflighthpc/metal-server
 #===============================================================================
 
-require 'spec_helper'
-require 'shared_examples/system_path_creater'
-require 'shared_examples/system_path_updater'
-require 'shared_examples/system_path_deleter'
-
-RSpec.describe Kickstart do
-  include_context 'with_system_path_subject'
-
-  it_behaves_like 'system path creater'
-  it_behaves_like 'system path updater'
-  it_behaves_like 'system path deleter'
-
-  describe 'GET /kickstarts/:id/blob' do
-    context 'without credentials' do
+RSpec.shared_examples 'system path updater' do
+  describe 'PATCH update' do
+    context 'with user credentials' do
       before(:all) do
         FakeFS.clear!
+        user_headers
         create_subject_and_system_path
-        unknown_headers
-        get subject_api_path('blob')
+        patch subject_api_path, subject_api_body
       end
 
-      it 'returns ok' do
-        expect(last_response).to be_ok
-      end
-
-      it 'returns the file' do
-        expect(last_response.body).to eq(File.read(subject.system_path))
+      it 'returns forbidden' do
+        expect_forbidden
       end
     end
 
-    context 'with a file' do
-      context 'with a user token' do
-        before(:all) do
-          FakeFS.clear!
-          create_subject_and_system_path
-          user_headers
-          get subject_api_path('blob')
-        end
+    context 'with admin credentials without an entry' do
+      before(:all) do
+        FakeFS.clear!
+        admin_headers
+        patch subject_api_path, subject_api_body(payload: 'some test payload')
+      end
 
-        it 'returns ok' do
-          expect(last_response).to be_ok
-        end
+      it 'returns not found' do
+        expect(last_response).to be_not_found
+      end
 
-        it 'returns the file' do
-          expect(last_response.body).to eq(File.read(subject.system_path))
-        end
+      it 'does not create the meta file' do
+        expect(File.exists? subject.path).to be(false)
+      end
+
+      it 'does not create the system file' do
+        expect(File.exists? subject.system_path).to be(false)
+      end
+    end
+
+    context 'with admin credentials and entry but without a payload' do
+      def original_payload
+        'I am the original file content'
+      end
+
+      before(:all) do
+        FakeFS.clear!
+        admin_headers
+        create_subject_and_system_path
+        File.write(read_subject.system_path, original_payload)
+        patch subject_api_path, subject_api_body
+      end
+
+      it 'returns okay' do
+        expect(last_response).to be_ok
+      end
+
+      it 'does not modify the system file' do
+        expect(File.read subject.system_path).to eq(original_payload)
       end
     end
   end
 end
-
