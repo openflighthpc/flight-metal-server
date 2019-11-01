ff5243fab46854529818ea73bbcb59c5caee22b8
william <william.mccumstie@gmail.com>
2019-11-01 11:40:22 +0000

    Move the managed file header to a shared location

--- lib/metal_server/dhcp_paths.rb
+++ lib/metal_server/dhcp_paths.rb
@@ -30,43 +30,10 @@
 require 'sinja'
 require 'open3'
 
+require 'metal_server/managed_file'
 require 'metal_server/restorer'
 
 module MetalServer
-  MANAGED_FILE_COPYRIGHT = <<~TEXT
-#==============================================================================
-# Copyright (C) 2019-present Alces Flight Ltd.
-#
-# This file is part of Metal Server.
-#
-# This program and the accompanying materials are made available under
-# the terms of the Eclipse Public License 2.0 which is available at
-# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
-# terms made available by Alces Flight Ltd - please direct inquiries
-# about licensing to licensing@alces-flight.com.
-#
-# Metal Server is distributed in the hope that it will be useful, but
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
-# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
-# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
-# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
-# details.
-#
-# You should have received a copy of the Eclipse Public License 2.0
-# along with Flight Cloud. If not, see:
-#
-#  https://opensource.org/licenses/EPL-2.0
-#
-# For more information on Metal Server, please visit:
-# https://github.com/openflighthpc/metal-server
-#===============================================================================
-
-#
-# This file has been rendered by OpenFlightHPC - Metal Server
-# Any changes to this file maybe lost
-#
-  TEXT
-
   DhcpPaths = Struct.new(:base, :version) do
     def self.current(base)
       new(base, DhcpCurrent.new(base).id)
